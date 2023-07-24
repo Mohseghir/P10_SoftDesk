2b1d5bc2c78c5c80bdbd13097303088b925d4c91
Mohseghir <baghaoui@homtail.com>
2023-07-24 13:14:01 +0200

    update

--- P10_SoftDesk/urls.py
+++ P10_SoftDesk/urls.py
@@ -41,14 +41,4 @@ urlpatterns = [
     path('signup/', RegisterAPIView.as_view(), name='signup'),
     path('login/', TokenObtainPairView.as_view(), name='token'),
 ]
-"""urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include(projects_router.urls)),
-    path("", include(users_router.urls)),
-    path("", include(issues_router.urls)),
-    path("", include(comments_router.urls)),
-    path("signup/", RegisterAPIView.as_view()),
-    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-]"""
-
 
